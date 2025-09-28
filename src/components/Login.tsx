22854a2a0cc15a37df4bbf1b538722f336e9e4fb
Dhanushka <dhanushkamcr@gmail.com>
2025-09-28 18:56:23 -0500

    Created Login.tsx

---
+++ src/components/Login.tsx
@@ -0,0 +1,68 @@
+import axios from "axios";
+import type React from "react";
+import { useState } from "react";
+import User from './User';
+
+const Login: React.FC = () =>{
+    const [email, setEmail] = useState('eve.holt@reqres.in');
+    const [password, setPassword] = useState('');
+    const [token, setToken] = useState('');
+
+    const handleLogin = async(e: React.FormEvent) =>{
+        e.preventDefault ();
+
+        try{
+            const response = await axios.post('https://reqres.in/api/login',
+            {
+                email,
+                password
+            },
+            {
+                headers: {'x-api-key': 'reqres-free-v1'}
+            });
+
+            console.log("Returned data: ", response.data)
+            const jwtToken = response.data.token;
+
+            setToken(jwtToken)
+
+            sessionStorage.setItem('jwtToken_key', jwtToken);
+            // history.push('/dashboard');
+        } catch(error) {
+            console.error('Login failed:', error);
+        }
+    };
+
+    const logoutUser = () => {
+        sessionStorage.clear();
+        setToken("")
+    };
+
+    return(
+        <div>
+        <h2>Login</h2>
+        <h3>Must use this email: eve.holt@reqres.in  </h3>
+        <h4>The password can be anything</h4>
+        <form onSubmit={handleLogin}>
+            <div>
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" value={email} 
+                onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <div>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" value={password} 
+                onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <button type="submit">Login</button>
+        </form>
+        <button onClick={logoutUser}>Logout</button>
+        {token &&
+            <User />
+        }  
+        
+        </div>
+    );
+}
+
+export default Login;
